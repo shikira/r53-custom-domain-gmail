9867afa0f3c77bd71babc69ffb2ed076cf01f219
shikira <akira.shirasu@gmail.com>
2025-11-09 00:10:02 +0900

    feat: 1.3 CDK初期化

--- packages/cdk/bin/app.ts
+++ packages/cdk/bin/app.ts
@@ -1,2 +1,18 @@
 #!/usr/bin/env node
-export {};
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { AwsSolutionsChecks } from 'cdk-nag';
+import { EmailForwardingStack } from '../lib/stacks/email-forwarding-stack';
+
+const app = new cdk.App();
+
+new EmailForwardingStack(app, 'EmailForwardingStack', {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION || 'us-east-1',
+  },
+});
+
+cdk.Aspects.of(app).add(new AwsSolutionsChecks({ verbose: true }));
+
+app.synth();
